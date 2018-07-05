63049535cedba01b5d2b6ddfc419bf0b474a6a1c
Lowellll <yejin2651@naver.com>
2018-07-05 22:53:07 +0900

    use middleware
    third-party middleware를  사용해봅니다!
    (body-parser,compression)

--- Express/main.js
+++ Express/main.js
@@ -5,7 +5,17 @@ var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var qs = require('querystring');
+var bodyParser = require('body-parser');
+var compression = require('compression');
 
+// bodyparser middleware 사용
+// 사용자가 요청할 때(main.js) 마다 미들웨어가 실행됨. 
+// 사용자가 보낸 post 데이터를 내부적으로 분석함.
+app.use(bodyParser.urlencoded({extended : false}));
+
+// compression middleware 사용
+// 데이터의 크기를 압축해줌.
+app.use(compression());
 
 // get 메소드를 받는 함수
 app.get('/',function (request,response){ 
@@ -68,11 +78,19 @@ app.get('/create',function(request,response){
 });
 
 app.post('/create_process',function(request,response){
+  var post = request.body; // bodyParser 사용
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        response.redirect(`/page/${title}`);
+      });
+  
+  /*
   var body = '';
-  request.on('data', function(data){
+  request.on('data', function(data){ // 데이터가 추가될 때 마다 호출
       body = body + data;
   });
-  request.on('end', function(){
+  request.on('end', function(){ // end 이벤트가 발생했을 때 
       var post = qs.parse(body);
       var title = post.title;
       var description = post.description;
@@ -80,6 +98,7 @@ app.post('/create_process',function(request,response){
         response.redirect(`/page/${title}`);
       })
   });
+  */
 });
 
 app.get('/update/:pageId',function(request,response){
